d33c601a8614d7d3faf07a0157dc1274079a9d7a
farlom <stepan.farlom@gmail.com>
2023-04-10 17:15:39 +0300

    header in selector hotfix

--- resources/js/selector.js
+++ resources/js/selector.js
@@ -351,48 +351,14 @@ $(document).ready(function (e) {
           'class': 'wrapper__buttons',
         })
 
-        // var form = $('<form>',{
-        //   'method': 'POST',
-        // })
-
-        var hidden_x = $('<input>', {
-          'type': 'hidden',
-          'id': `x[${$('.canvas').children('.square').length - 1}]`,
-          'value': $(id).css('left')
-        })
-
-        var hidden_y = $('<input>', {
-          'type': 'hidden',
-          'name': 'y',
-          'wire:model': 'y',
-          'value': $(id).css('top')
-        })
-
-        var hidden_width = $('<input>', {
-          'type': 'hidden',
-          'name': 'width',
-          'wire:model': 'width',
-          'value': $(id).css('width')
-        })
-
-        var hidden_height = $('<input>', {
-          'type': 'hidden',
-          'name': 'height',
-          'wire:model': 'height',
-          'value': $(id).css('height')
-        })
-
         console.log($(id).css('left'))
         console.log(x_arr)
         // console.log(startcoordX)
-        var svgEdit = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="blue" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 align-middle me-2"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>'
-        var svgDelete = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 align-middle me-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>'
-        $(desMetrik).html($('input[name=color_selector]:checked').next().html())
-        $(numberEl).html($('.canvas').children('.square').length)
+        
+        
+        
 
-        // $(form).append(object)
-        $(object).append(numberEl)
-        $(object).append(desMetrik)
+
         $(obj).append(table_row)
         // $(obj).append('<tr id="table_row_0" class="table-row" style="background-color: #00000040;"><td class="number">1</td><td class="desmetr">Категория1</td><td class="table-action"><a class="button__editing" id="editing__button0" style="text-decoration: none"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="blue" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 align-middle"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a><a class="button__deletting" id="deletting__button0" style="text-decoration: none"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 align-middle me-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a></td></tr>')
         console.log($('#span_' + radio_checked_id).text())
@@ -422,17 +388,6 @@ $(document).ready(function (e) {
         // $(object).append(textbox)
         // $(object).append('<label for="testtext">123</label>')
 
-        $(buttonDeleting).append(svgDelete)
-        $(buttonEditing).append(svgEdit)
-
-        $(wrapperTools).append(buttonDeleting, buttonEditing)
-
-        $(object).append(wrapperTools)
-
-        $(object).attr('id', `prev__elemnt__objects${$('.canvas').children('.square').length - 1}`)
-
-
-        $(prevobjects).append(object)
         break
     }
 
