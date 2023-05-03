a15a33b9b05c4514af7833db134534446ab7cc93
farlom <stepan.farlom@gmail.com>
2023-05-03 19:18:28 +0300

    Фото по центру

--- resources/js/selector.js
+++ resources/js/selector.js
@@ -1,37 +1,4 @@
 $(document).ready(function (e) {
-
-  console.log($('div.container').css('height'))
-  console.log($('div.container').css('width'))
-
-
-
-  // if ($('#hiddenX').val() == '') {
-  //   var x_arr = []
-  //   var y_arr = []
-  //   var width_arr = []
-  //   var height_arr = []
-  //   var categories_arr = []
-  // }
-  // else {
-
-  //   var x_arr = $('#hiddenX').val().split(',')
-  //   var y_arr = $('#hiddenY').val().split(',')
-  //   var width_arr = $('#hiddenWidth').val().split(',')
-  //   var height_arr = $('#hiddenHeight').val().split(',')
-  //   var categories_arr = $('#hiddenCategory').val().split(',')
-  // }
-  var zoom = $('.canvas')
-
-
-  var width_window = parseFloat($('.position_image').css('width'))
-  console.log(width_window)
-  $(zoom).css('transform', "translate(1,1,1,1,1,1)")
-  
-  console.log($(zoom).css('transform'))
-  
-  
-  
-  var delete_arr = []
   const Statuses = Object.freeze({
     CreatingElements: 1,
     EditingElements: 2,
@@ -56,39 +23,18 @@ $(document).ready(function (e) {
   $(canvas).css("width", img.width);
   $(canvas).css("height", img.height);
 
-  // while (
-  //   img.height * scale > parseInt($('div#text').css('height')) || img.width * scale > parseInt($('div.container').css('width'))
-  // ) {
-  //   scale -= 0.05
-  // }
-
-  console.log(parseFloat($('.position_image').css('width')), 'aaaaaaaaaaaaaaaa')
-  var width_position_wrapper = parseFloat($('.position_image').css('width'))
-  var width_position_image = parseFloat($('#image').css('width'))
-
-
-  var height_position_wrapper = parseFloat($('.position_image').css('height'))
-  var height_position_image = parseFloat($('#image').css('height'))
-
-
-  var widthdist = Math.abs((width_position_wrapper - width_position_image)/2)
-  var heightdist = Math.abs((height_position_image - height_position_wrapper)/2)
-
-
-  if (widthdist > heightdist){
-    $('#image').css('height', height_position_wrapper)
+  while (
+    img.height * scale > parseInt($(canvas).css('height')) || img.width * scale > parseInt($(canvas).css('width'))
+  ) {
+    scale -= 0.05
+    // console.log(scale)
   }
-  else
-  {
-    $('#image').css('width', width_position_wrapper)
-
-  }
-
-
-  $(canvas).css('transform', 'scale('+scale+')')
-  $(canvas).css('left',`${parseFloat($('.position_image').css('width'))/2 - parseFloat($('#image').css('width'))/2}px`)
-  
-  // text.style.transform = text.style.WebkitTransform = text.style.MsTransform = 'scale(' + 1 + ')';
+  console.log(parseFloat($(text).css('width')), parseFloat($('.img__current').css('width')), parseFloat($(text).css('width')) / 2 - parseFloat($('.img__current').css('width')) / 2)
+  $(canvas).css('left', parseFloat($(text).css('width')) / 2 - parseFloat($('.img__current').css('width')) / 2)
+  // pointX = Math.abs((parseFloat($(canvas).css('width')) - parseFloat($(text).css('width')) * 2)) 
+  $(canvas).css('transform', 'scale('+scale+') translate('+pointX+'px, 0px)')
+  console.log((parseFloat($(canvas).css('width')) - parseFloat($(text).css('width')) * scale ) / 2)
+  // alert($(text).css('width'))
   var min_scale = scale
   const square = 'square'
   const prev__elemnt__objects = 'prev__elemnt__objects'
@@ -150,7 +96,7 @@ $(document).ready(function (e) {
           $(objects[i]).attr('id', prev__elemnt__objects + i)
           $(objects[i]).children('.number').html(i + 1)
         }
- 
+
 
         break
     }
@@ -464,6 +410,7 @@ $(document).ready(function (e) {
             // translate when resizing from top or left edges
             x += event.deltaRect.left * (1 / scale)
             y += event.deltaRect.top * (1 / scale)
+
             target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
 
             target.setAttribute('data-x', x)
@@ -699,6 +646,7 @@ $(document).ready(function (e) {
 
 
   
+  var zoom = $('.canvas')
 
   addOnWheel(text, function (e) {
     var delta = e.deltaY || e.detail || e.wheelDelta;
@@ -715,9 +663,7 @@ $(document).ready(function (e) {
 
       pointX = e.layerX - xs * scale
       pointY = e.layerY - ys * scale
-            // console.log($('.position_image').css('width'));
-
-      $(zoom).css('transform', "translate(" + pointX  + "px, " + pointY + "px) scale(" + scale + ")")
+      $(zoom).css('transform', "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")")
     e.preventDefault();
   }); 
 })
